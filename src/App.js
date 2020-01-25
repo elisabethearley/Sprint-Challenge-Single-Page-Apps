ab2e6853acecd57f646db1f6eb8d48441a5e718a
Elisabeth <elisabethearley@gmail.com>
2020-01-25 13:39:58 -0500

    added axios & useState to App.js, created axios call for Rick&Morty api, going over data to choose elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+//import { Route } from "react-router-dom";
+import axios from "axios";
 import Header from "./components/Header.js";
 
 
 export default function App() {
+  const [chars, setChars] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(response => {
+      console.log(response.data.results);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }, []);
+  
   return (
     <main>
       <Header />
